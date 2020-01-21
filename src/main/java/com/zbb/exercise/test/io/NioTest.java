a86a78e938b61e53aa9c460fb4c5cf346ad84f15
775170427@qq.com <775170427@qq.com>
2020-01-21 11:50:10 +0800

    NIO

---
+++ src/main/java/com/zbb/exercise/test/io/NioTest.java
@@ -0,0 +1,37 @@
+package com.zbb.exercise.test.io;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest {
+
+    @Test
+    public void readFile() throws IOException {
+
+        RandomAccessFile aFile = new RandomAccessFile("D:\\1.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        //create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf); //read into buffer.
+        while (bytesRead != -1) {
+
+            buf.flip();  //make buffer ready for read
+
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get()); // read 1 byte at a time
+            }
+
+            buf.clear(); //make buffer ready for writing
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+
+
+}
